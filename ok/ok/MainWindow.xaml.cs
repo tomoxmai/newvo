9b35afbff565bda0722f85a38c0c9ba9800347c5
Alumno <Alumno@PC15>
2019-10-07 10:16:58 -0500

    ggg

--- ok/ok/MainWindow.xaml.cs
+++ ok/ok/MainWindow.xaml.cs
@@ -55,9 +55,47 @@ namespace ok
                 gvDatos.Visibility = System.Windows.Visibility.Hidden;
             }
         }
+        private void  LimpiarTodo()
+        {
+            txtId.Text = "";
+            txtnombre.Text = "";
+            cbGenero.SelectedIndex = 0;
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
+        }
 
         private void BtnNuevo_Click(object sender, RoutedEventArgs e)
         {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            if(txtId.Text!="")
+            {
+                if(cbGenero.Text!="Selecciona Genero")
+                {
+                    cmd.CommandText = "insert into Progra(Id,Nombre,Genero,Telefono,Direccion)" + "Values(" + txtId.Text + ",'" + txtnombre.Text + ",'" + cbGenero.Text + ",'" + txtTelefono.Text + ",'" + txtDireccion.Text + ")'";
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Alumno agregado correctamente...");
+                    LimpiarTodo();
+                }
+                else
+                {
+                    MessageBox.Show("Favor de seleccionar el genero...");
+                }
+            }
+            else
+            {
+                cmd.CommandText = "update Progra set Nombre='" + txtnombre.Text + "',Genero='" + cbGenero.Text + "',Telefono" + txtTelefono.Text + ",'Direccion='" + txtDireccion.Text + "'where Id=" + txtId.Text;
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Datos del alumno Actualizados...");
+                LimpiarTodo();
+
+
+            }
 
         }
 
